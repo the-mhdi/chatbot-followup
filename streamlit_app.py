1ee070bb54ec6ff511d0aa6aa1ee1b5a183d2fd3
Mehdi karimi <127137582+the-mhdi@users.noreply.github.com>
2024-12-21 17:07:29 +0000

    hiiiiiiiiiii

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,7 @@ import google.generativeai as genai
 # Show title and description.
 st.title("💬 Chatbot")
 st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
+    "This is a simple chatbot"
 )
 
 # Ask user for their OpenAI API key via `st.text_input`.
